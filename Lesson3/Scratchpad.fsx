a3b74bb59b0ba9be1327f122879fd5170feeb3fb
heronbpv <heronbpeixoto@gmail.com>
2019-05-03 17:16:46 -0300

    Part 1 of Try this: the countWords function behaves as intended. Tests included.

--- Lesson3/Scratchpad.fsx
+++ Lesson3/Scratchpad.fsx
@@ -5,3 +5,20 @@ let greetPerson name age =
     sprintf "Hello, %s. You are %d years old" name age
 
 let greeting = greetPerson "Fred" 25
+
+
+//Try this
+let countWords (text:string) =
+    let count = if text |> isNull then 0 else text.Split([|' '|], System.StringSplitOptions.RemoveEmptyEntries).Length 
+    count
+
+//Testing countWords in a REPL environment
+countWords "This is a test"
+countWords " "
+countWords null
+countWords "thisisatest"
+countWords "thisis   atestwith   toomanyspaces"
+countWords "thistest.separatesinaperiod"
+countWords "thistest. containsaperiod"
+
+//File creation
