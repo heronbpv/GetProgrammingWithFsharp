59ee7ff498ee6925655fbf234979ac9407f95c3c
heronbpv <heronbpeixoto@gmail.com>
2019-05-03 15:52:31 -0300

    Solutions to the "Try this" section, both as intended and a commented more functional one.

--- Lesson2/MyFirstSharpApp/MyFirstSharpApp/Program.fs
+++ Lesson2/MyFirstSharpApp/MyFirstSharpApp/Program.fs
@@ -5,4 +5,12 @@
 let main argv = 
     let items = argv.Length
     printfn "Passed in %d items: %A" items argv
+
+    for x = 1 to argv.Length do
+        printfn "Position: %d; Value: %s; Lenght: %d" (x) argv.[x-1] argv.[x-1].Length
+
+    //A proper, functional way of doing the "Try this" of lesson 2
+//    argv
+//    |> Array.iteri (fun x y -> printfn "Position: %d; Value: %s; Lenght: %d" (x + 1) y y.Length) 
+
     0 // return an integer exit code
