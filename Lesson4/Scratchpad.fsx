af0882a622b619ee7ae02a2132cb95fc5ddd2ffa
heronbpv <heronbpeixoto@gmail.com>
2019-05-04 16:37:58 -0300

    First ideation of lesson 4 now you try exercise. Scopes are now more tight both for the downloaded content and to the form creation.

--- Lesson4/Scratchpad.fsx
+++ Lesson4/Scratchpad.fsx
@@ -4,14 +4,19 @@ let y = 1
 let rng = System.Random 1
 let callRngsus () = rng.Next 100
 
-//Winforms example
+//@4.2.2
 open System
 open System.Net
 open System.Windows.Forms
 
-let webClient = new WebClient()
-let fsharpOrg = webClient.DownloadString(Uri "http://www.giantitp.com/forums/forumdisplay.php?26-Gaming-(Other)")
-let browser = new WebBrowser(ScriptErrorsSuppressed = true, Dock = DockStyle.Fill, DocumentText = fsharpOrg)
-let form = new Form(Text = "Hello from GitP!")
-form.Controls.Add browser
+let fsharpOrg = 
+    let webClient = new WebClient()
+    webClient.DownloadString(Uri "http://www.giantitp.com/forums/forumdisplay.php?26-Gaming-(Other)")
+
+let form = 
+    let browser = new WebBrowser(ScriptErrorsSuppressed = true, Dock = DockStyle.Fill, DocumentText = fsharpOrg)
+    let x = new Form(Text = "Hello from GitP!")
+    x.Controls.Add browser
+    x
+
 form.Show()
