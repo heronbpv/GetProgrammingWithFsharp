e860e10736287f2386ac3f52e036fbc733488c8b
heronbpv <heronbpeixoto@gmail.com>
2019-05-04 16:18:46 -0300

    Lesson 4 first big now you try exercise, after topic 4.2.2. This is the initial commit on scratchpad, after comment @4.2.2. Changed the URI as the fsharp.org link was using https, and the lack of a cert is messing the connection.

--- Lesson4/Scratchpad.fsx
+++ Lesson4/Scratchpad.fsx
@@ -3,3 +3,15 @@ let y = 1
 
 let rng = System.Random 1
 let callRngsus () = rng.Next 100
+
+//Winforms example
+open System
+open System.Net
+open System.Windows.Forms
+
+let webClient = new WebClient()
+let fsharpOrg = webClient.DownloadString(Uri "http://www.giantitp.com/forums/forumdisplay.php?26-Gaming-(Other)")
+let browser = new WebBrowser(ScriptErrorsSuppressed = true, Dock = DockStyle.Fill, DocumentText = fsharpOrg)
+let form = new Form(Text = "Hello from GitP!")
+form.Controls.Add browser
+form.Show()
