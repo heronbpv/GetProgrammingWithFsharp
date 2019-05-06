614a485cea068b9a6a361b3ef4441108945555a8
heronbpv <heronbpeixoto@gmail.com>
2019-05-06 16:49:07 -0300

    Solutions to the now youtry 5.3. Code is commented to avoid the compilation erros that ensues.

--- Lesson5/Scratchpad.fsx
+++ Lesson5/Scratchpad.fsx
@@ -12,14 +12,65 @@ let add a b =
 //add 1 2
 
 //@Now you try 5.3
-let sayHello(someValue) =
-    let innerFunction(number) = 
-        if number > 10 then "Isaac"
-        elif number > 20 then "Fred"
-        else "Sara"
-    let resultOfInner = 
-        if someValue < 10.0 then innerFunction(5)
-        else innerFunction(15)
-    "Hello " + resultOfInner
+//Original version
+//let sayHello(someValue) =
+//    let innerFunction(number) = 
+//        if number > 10 then "Isaac"
+//        elif number > 20 then "Fred"
+//        else "Sara"
+//    let resultOfInner = 
+//        if someValue < 10.0 then innerFunction(5)
+//        else innerFunction(15)
+//    "Hello " + resultOfInner
+//
+//let result = sayHello(10.5)
 
-let result = sayHello(10.5)
+(** 
+ * Scenario 1 - change string Isaac for 123. Result: the compiler infers the ifelif expression to return an int (since it's the type of the first return), so all other returning values
+ * must be int as well, hence the compiler error. Plus, at the call site "Hello " + resultOfInner, since resultOfInner now is an int thanks to the changes to innerFunction (which now 
+ * returns int), a type mismatch error occurs.
+ **)
+//let sayHello(someValue) =
+//    let innerFunction(number) = 
+//        if number > 10 then 123
+//        elif number > 20 then "Fred"
+//        else "Sara"
+//    let resultOfInner = 
+//        if someValue < 10.0 then innerFunction(5)
+//        else innerFunction(15)
+//    "Hello " + resultOfInner
+//
+//let result = sayHello(10.5)
+
+(**
+ * Scenario 2 - change string Fred to 123. Result: since the type inference mechanism operates from top to bottom, left to right, the return type of innerFunction is inferred to be int,
+ * as is the return of the first brach of the ifelif expression. So, once the value of the string was changed to an int in the second expression, the error is now local only, since the
+ * string return takes precedence in determining the returning type.
+ **)
+//let sayHello(someValue) =
+//    let innerFunction(number) = 
+//        if number > 10 then "Isaac"
+//        elif number > 20 then 123
+//        else "Sara"
+//    let resultOfInner = 
+//        if someValue < 10.0 then innerFunction(5)
+//        else innerFunction(15)
+//    "Hello " + resultOfInner
+//
+//let result = sayHello(10.5)
+
+(**
+ * Scenario 3 - change float 10.0 to 10. Result: since the first usage of someValue refers to it as an int (by virtue of using the comparison operator on a basic number, which is implied
+ * to be int by the compiler rules engine), the signature of the sayHello function changes to int -> string, instead of float -> string, thus resulting in the error at it's call site.
+ **)
+//let sayHello(someValue) =
+//    let innerFunction(number) = 
+//        if number > 10 then "Isaac"
+//        elif number > 20 then "Fred"
+//        else "Sara"
+//    let resultOfInner = 
+//        if someValue < 10 then innerFunction(5)
+//        else innerFunction(15)
+//    "Hello " + resultOfInner
+//
+//let result = sayHello(10.5)
