127866c2646d86de31d8d2ef6bb3e35e76563d6f
heronbpv <heronbpeixoto@gmail.com>
2019-05-07 13:57:31 -0300

    Topic 8.4.1 resolution.

--- Lesson8-Capstone1/scratchpad.fsx
+++ Lesson8-Capstone1/scratchpad.fsx
@@ -3,10 +3,13 @@ open System
 /// Gets the distance to a given destination 
 let getDistance (destination) =
     if destination = "Gas" then 10
-    // fill in the blanks!
+    elif destination = "Home" then 25
+    elif destination = "Stadium" then 25
+    elif destination = "Office" then 50
     else failwith "Unknown destination!"
 
 // Couple of quick tests
-getDistance("Home") = 25
-getDistance("Stadium") = 25
+let isHome25 = getDistance("Home") = 25
+let isStadium25 = getDistance("Stadium") = 25
+let isGas10 = getDistance("Gas") = 10
 
