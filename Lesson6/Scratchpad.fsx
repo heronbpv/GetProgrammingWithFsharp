92fa0f5212ddfd19fc16d78facc982add4c82301
heronbpv <heronbpeixoto@gmail.com>
2019-05-07 10:30:26 -0300

    Now you try 6.3.1. Code after comment @Now you try 6.3.1.

--- Lesson6/Scratchpad.fsx
+++ Lesson6/Scratchpad.fsx
@@ -7,4 +7,16 @@ form.Height <- 400
 form.Text <- "Hello from F#!"
 
 let form2 = new Form(Text = "Hello from F#!", Width = 300, Height = 300)
-form2.Show()
+form2.Show
+
+//@Now you try 6.3.1
+let mutable petrol = 100.0
+let drive(distance) = 
+    if distance = "far" then petrol <- petrol / 2.0
+    elif distance = "medium" then petrol <- petrol - 10.0
+    else petrol <- petrol - 1.0
+
+drive("far")
+drive("medium")
+drive("short")
+petrol
