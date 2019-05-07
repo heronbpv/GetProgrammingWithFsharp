b4386737d6bbb5e903599a8fca8cce83d42cf319
heronbpv <heronbpeixoto@gmail.com>
2019-05-07 10:51:21 -0300

    Now you try 6.3.2 initial setup. From thereupon, assume that now you try sections are on scratchpads, under de respective comment, with an @ symbol.

--- Lesson6/Scratchpad.fsx
+++ Lesson6/Scratchpad.fsx
@@ -10,13 +10,24 @@ let form2 = new Form(Text = "Hello from F#!", Width = 300, Height = 300)
 form2.Show
 
 //@Now you try 6.3.1
-let mutable petrol = 100.0
-let drive(distance) = 
-    if distance = "far" then petrol <- petrol / 2.0
-    elif distance = "medium" then petrol <- petrol - 10.0
-    else petrol <- petrol - 1.0
+//let mutable petrol = 100.0
+//let drive(distance) = 
+//    if distance = "far" then petrol <- petrol / 2.0
+//    elif distance = "medium" then petrol <- petrol - 10.0
+//    else petrol <- petrol - 1.0
+//
+//drive("far")
+//drive("medium")
+//drive("short")
+//petrol
 
-drive("far")
-drive("medium")
-drive("short")
-petrol
+//@Now you try 6.3.2
+let drive(petrol, distance) = 
+    if distance = "far" then petrol / 2.0
+    elif distance = "medium" then petrol - 10.0
+    else petrol - 1.0
+
+let petrol = 100.0
+let firstState = drive(petrol, "far")
+let secondState = drive(firstState, "medium")
+let finalState = drive(secondState, "short")
