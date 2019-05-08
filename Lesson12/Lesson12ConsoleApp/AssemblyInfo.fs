f1eb5fe8aa8fbf418f761d49bac17940a850f944
heronbpv <heronbpeixoto@gmail.com>
2019-05-08 14:47:57 -0300

    Now you try 12.2.

---
+++ Lesson12/Lesson12ConsoleApp/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Lesson12ConsoleApp.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Lesson12ConsoleApp")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Lesson12ConsoleApp")>]
+[<assembly: AssemblyCopyright("Copyright ©  2019")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("a4f357ee-2a94-42da-adf5-7541821acf28")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Lesson12/Lesson12ConsoleApp/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open Domain
+open Operations
+
+[<EntryPoint>]
+let main _ = 
+    let joe = { FirstName = "joe"; LastName = "bloggs"; Age = 21 }
+
+    if joe |> isOlderThan 18 then printfn "%s is an adult!" joe.FirstName
+    else printfn "%s is a child." joe.FirstName
+
+    0 // return an integer exit code
