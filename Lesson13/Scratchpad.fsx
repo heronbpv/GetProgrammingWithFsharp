8da81b461202e5e4511a1ce03fc73e64b444c4aa
heronbpv <heronbpeixoto@gmail.com>
2019-05-08 16:04:56 -0300

    Now you try 13.2.1.

---
+++ Lesson13/Scratchpad.fsx
@@ -0,0 +1,14 @@
+//@Now you try 13.2.1
+type Customer = 
+    { Age : int }
+
+let where filter customers = 
+    seq {
+        for customer in customers do
+            if filter customer then
+                yield customer
+    }
+let customers = [ {Age = 21}; {Age = 35}; {Age = 36} ]
+let isOver35 customer = customer.Age > 35
+customers |> where isOver35
+customers |> where (fun customer -> customer.Age > 35)
