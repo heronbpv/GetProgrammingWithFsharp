71c332a6e934388a87094b17f56ef2380ded7c0a
heronbpv <heronbpeixoto@gmail.com>
2019-05-08 17:17:57 -0300

    Modified the functions to be more function and removed the hardcoded values inside.

--- Lesson14-Capstone2/Capstone2/Scratchpad.fsx
+++ Lesson14-Capstone2/Capstone2/Scratchpad.fsx
@@ -17,7 +17,12 @@ let bobAccount = {Id = Guid.NewGuid(); Balance = 100M; Owner = bob}
 let janeAccount = {Id = Guid.NewGuid(); Balance = 70M; Owner = jane}
 
 //Functions that operate on said types
-let deposit (amount:decimal) (account:Account) :Account =
-    { Id = Guid.Empty; Owner = {Id = Guid.Empty; Name = "Sam"; Age = 25}; Balance = 10M }
-let withdraw (amount:decimal) (account:Account) :Account =
-    { Id = Guid.Empty; Owner = {Id = Guid.Empty; Name = "Sam"; Age = 25}; Balance = 10M }
+let deposit amount account =
+    { account with Balance = account.Balance + amount }
+let withdraw amount account =
+    if amount <= account.Balance then { account with Balance = account.Balance - amount }
+    else account
+
+//Testing the functions
+janeAccount |> deposit 50M |> withdraw 25M |> deposit 10M
+bobAccount |> withdraw 100M |> withdraw 10M |> deposit 25M |> withdraw 175M
