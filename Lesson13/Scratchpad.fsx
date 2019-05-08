cab29c4d90e0a8258051874dd1e12ff0af6ea6a3
heronbpv <heronbpeixoto@gmail.com>
2019-05-08 16:23:14 -0300

    Now you try 13.3 first commit.

--- Lesson13/Scratchpad.fsx
+++ Lesson13/Scratchpad.fsx
@@ -12,3 +12,13 @@ let customers = [ {Age = 21}; {Age = 35}; {Age = 36} ]
 let isOver35 customer = customer.Age > 35
 customers |> where isOver35
 customers |> where (fun customer -> customer.Age > 35)
+
+//@Now you try 13.3
+open System
+let printCustomerAge customer = 
+    if customer.Age < 12 then Console.WriteLine("Child!")
+    elif customer.Age >= 12 && customer.Age < 18 then Console.WriteLine("Teenager!")
+    else Console.WriteLine("Adult!")
+
+let customers2 = [{Age = 8}; {Age = 17}; {Age = 36}]
+customers2 |> List.iter printCustomerAge
