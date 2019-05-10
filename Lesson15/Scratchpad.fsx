925f640538eb87d4620a964d776fa74c0c81e071
heronbpv <heronbpeixoto@gmail.com>
2019-05-10 14:38:04 -0300

    Now you try 15.1 initial commit. The proposed types and collection have been created and commented.

---
+++ Lesson15/Scratchpad.fsx
@@ -0,0 +1,24 @@
+//@Now you try 15.1
+//Which teams won the most away games in this season?
+//Data structure and creation function
+type FotballResult = 
+    { HomeTeam : string
+      AwayTeam : string
+      HomeGoals : int
+      AwayGoals : int }
+
+let create (ht, hg) (at, ag) = 
+    { HomeTeam = ht
+      AwayTeam = at
+      HomeGoals = hg
+      AwayGoals = ag }
+
+//Results of this season's games
+let results = 
+    [ create ("Messiville", 1) ("Ronaldo City", 2)
+      create ("Messiville", 1) ("Bale Town", 3)
+      create ("Bale Town", 3) ("Ronaldo City", 1)
+      create ("Bale Town", 2) ("Messiville", 1)
+      create ("Ronaldo City", 4) ("Messiville", 2)
+      create ("Ronaldo City", 2) ("Bale Town", 2) ]
+
