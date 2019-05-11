755de587baa500221311e0e324911237779a4e25
heronbpv <heronbpeixoto@gmail.com>
2019-05-10 21:50:23 -0300

    Next mapping function: iter. Example + comments.

--- Lesson16/Scratchpad.fsx
+++ Lesson16/Scratchpad.fsx
@@ -1,11 +1,19 @@
 //Mapping functions -> convert (map) a collection of a certain type to a collection of another type. 
 //Sometimes, the item type may be referred to as the item shape, hence calling these shapeshifting functions seems cool as fuck.
 
-//@Map: Signature ('T -> 'U) -> 'T list -> 'U list; LINQ equivalent: Select; Input and output collection sizes are the same
+//@Map: Signature ('T -> 'U) -> 'T list -> 'U list; LINQ equivalent: Select; Obs.: Input and output collection sizes are the same.
 let numbers = [1..10]
 let timesTwo n = n * 2
-let outputImperative = ResizeArray() //Example imperative ideia.
+let outputImperative = ResizeArray() //Example imperative ideia: creates a new collection based on a transformation of the elements of another.
 for number in numbers do
     outputImperative.Add (number |> timesTwo)
 outputImperative
 let outputFunctional = numbers |> List.map timesTwo //Equivalent functional ideia.
+
+//@Iter: Signature ('T -> unit) -> 'T list -> unit; LINQ equivalent: N/A; Obs.: Applies side-effect to each element. Equivalent to a for each loop.
+[("Isaac", "London")
+ ("Sara", "Birmingham")
+ ("Tim", "London")
+ ("Michelle", "Manchester")]
+|> List.iter (fun (name, city) -> printfn "Hello, %s" name)
+
