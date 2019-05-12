d308484b02e9a6e00f6883860328f70beaf03a71
heronbpv <heronbpeixoto@gmail.com>
2019-05-11 22:22:06 -0300

    Notes on the Map collection type.

--- Lesson17/Scratchpad.fsx
+++ Lesson17/Scratchpad.fsx
@@ -27,3 +27,18 @@ inv2.Remove("Bananas") //These last two throw exceptions.
 
 //Neat little trick: initializing a dictionary using dict to avoid the create then fill pattern of the BCL type.
 ["Apples", 10; "Bananas", 20; "Grapes", 15] |> dict |> Dictionary
+
+//@Map (the collection type, not the function)
+//This collection is immutable by default, and has support for some of the F# collection operations (map, filter, partition, etc.)
+let inv3 = ["Apples", 0.33; "Oranges", 0.23; "Bananas", 0.45] |> Map.ofList
+let apples = inv3.["Apples"]
+let pineapples = inv3.["Pineapples"] //Throws an exception -> KeyNotFound
+
+let newInv3 = 
+    inv3
+    |> Map.add "Pineapples" 0.87
+    |> Map.remove "Apples"
+
+let cheapFruit, expensiveFruit = 
+    newInv3
+    |> Map.partition (fun fruit cost -> cost < 0.3) //The arguments are the key -> value pair of the map, curried thanks to the predicate definition.
