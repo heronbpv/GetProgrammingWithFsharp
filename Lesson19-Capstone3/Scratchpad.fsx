6b1439ca2609dfbad0b547d180c4b38a4693f6dc
heronbpv <heronbpeixoto@gmail.com>
2019-05-17 15:23:15 -0300

    Initial setup of the final pipeline on the scratchpad, as per the books instructions.

--- Lesson19-Capstone3/Scratchpad.fsx
+++ Lesson19-Capstone3/Scratchpad.fsx
@@ -5,3 +5,18 @@ open Capstone3.Operations
 open Capstone3.Domain
 open System
 
+//Pipeline functions
+let isValidCommand (command:char) = if command = 'w' then true else false
+let isStopCommand (command:char) = false
+let getAmount (command:char) = command, 0M
+let processCommand (account:Account) (command:char, amount:decimal) = account
+
+//Testing the pipeline
+let openingAccount = {Owner = {Name = "Isaac"}; Balance = 0M; AccountId = Guid.Empty} //The test account
+let account = 
+    let commands = ['d'; 'w'; 'z'; 'f'; 'd'; 'x'; 'w']
+    commands
+    |> Seq.filter isValidCommand
+    |> Seq.takeWhile (not << isStopCommand) //operator "<<" read as "compose left with" or "compose backwards from", in regards to the flow direction of output to input.
+    |> Seq.map getAmount
+    |> Seq.fold processCommand openingAccount
