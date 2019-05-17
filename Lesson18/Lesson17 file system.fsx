835f82d69044540a3c37d4512341304986f1db06
heronbpv <heronbpeixoto@gmail.com>
2019-05-17 12:09:57 -0300

    Try this resolution. The current format of the filter function is thanks to the linter, pointing out that my original lambda on the Seq.filter could be reduced to a simple function composition using the compose forward/compose right with function (symbol: >>). The rules where adjusted a little based on the filesinside the test directories, in order to return both one empty and one collection with more than one item.

--- Lesson18/Lesson17 file system.fsx
+++ Lesson18/Lesson17 file system.fsx
@@ -11,8 +11,8 @@ type Rule = FileInfo -> bool * string
 
 open System
 let rules :Rule list = 
-    [fun file -> file.Length <= 100L, "File size must be less than 100KB."
-     fun file -> file.Extension = ".txt", "File type must be txt."
+    [fun file -> file.Length <= 800000L, "File size must be less than 800.000 bytes."
+     fun file -> file.Extension.Equals(".xml", StringComparison.OrdinalIgnoreCase), "File type must be xml."
      fun file -> DateTime.op_LessThan(file.CreationTime, DateTime.Today), "File must have been created before today."] //No, that last rule does't make sense, I know...
 
 //Aggregator, now using reduce instead of fold; adapted from the scratchpad from lesson 18.
@@ -37,4 +37,13 @@ let validateReduce (rules:Rule list) =
 let set1 = getAllFilesFromDir @"D:\Programacao\GetProgrammingWithFsharp\Lesson14-Capstone2\Capstone2\obj"
 let set2 = getAllFilesFromDir @"D:\Programacao\GetProgrammingWithFsharp\Lesson14-Capstone2\Capstone2\bin"
 
+//A test used to help identify the best function to make a DateTime comparison, to be used on the filters above.
 set1 |> Seq.map (fun file -> (file.CreationTime.ToString(), DateTime.UtcNow.ToString()) , DateTime.op_LessThan(file.CreationTime, DateTime.UtcNow))
+
+//Generates the filter as the result of the rules engine processing of the list of rules
+let filter = validateReduce rules >> fst //Applies the filters, then extracts the value of the first element of the resulting tuple, which is a boolean.
+
+//Tests
+let filteredSet1 = set1 |> Seq.filter filter |> List.ofSeq
+let filteredSet2 = set2 |> Seq.filter filter |> List.ofSeq
+//let nullSet = null |> Seq.filter filter |> List.ofSeq //Exception thrown by the Seq module function.
