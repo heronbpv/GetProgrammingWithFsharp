5be7b5e072e32951ba60fced0df775d80dc8dec1
heronbpv <heronbpeixoto@gmail.com>
2019-05-17 15:53:07 -0300

    Ported the logic from the scratchpad to Program.fs, as instructed in the book.

--- Lesson19-Capstone3/Program.fs
+++ Lesson19-Capstone3/Program.fs
@@ -4,6 +4,27 @@ open System
 open Capstone3.Domain
 open Capstone3.Operations
 
+//Pipeline functions
+///Checks whether the command is one of (d)eposit, (w)ithdraw, or e(x)it.
+let isValidCommand (command:char) = ['d'; 'w'; 'x'] |> List.contains command
+
+///Checks whether the command is the e(x)it command.
+let isStopCommand (command:char) = command = 'x'
+
+///Returns a tuple of the command and the associated predefined value, or zero.
+let getAmount (command:char) = 
+    match command with
+    | 'd' -> command, 50M
+    | 'w' -> command, 25M
+    | _ -> command, 0M
+
+///Applies the given pair of command and amount to the account in question.
+let processCommand (account:Account) (command:char, amount:decimal) = 
+    match command with
+    | 'd' -> deposit amount account
+    | 'w' -> withdraw amount account
+    | _ -> account
+
 [<EntryPoint>]
 let main _ =
     let name =
@@ -16,8 +37,12 @@ let main _ =
     let openingAccount = { Owner = { Name = name }; Balance = 0M; AccountId = Guid.Empty } 
 
     let closingAccount =
-        // Fill in the main loop here...
-        openingAccount
+        let commands = ['d'; 'w'; 'z'; 'f'; 'd'; 'x'; 'w']
+        commands
+        |> Seq.filter isValidCommand
+        |> Seq.takeWhile (not << isStopCommand) //operator "<<" read as "compose left with" or "compose backwards from", in regards to the flow direction of output to input.
+        |> Seq.map getAmount
+        |> Seq.fold processCommand openingAccount
 
     Console.Clear()
     printfn "Closing Balance:\r\n %A" closingAccount
