8e301bfd5c35f8b449882f165594e2d707642bd0
heronbpv <heronbpeixoto@gmail.com>
2019-05-16 22:18:38 -0300

    Now implementing the sum aggregation using the fold high order function (hof for short).

--- Lesson18/Scratchpad.fsx
+++ Lesson18/Scratchpad.fsx
@@ -49,3 +49,32 @@ let ``Max in an unordered list of number must be result in the highest number`` 
 let ``Max of an empty list is 0`` = max []
 //let ``Max in a list of nulls is a compilation error`` = max [null; null; null]
 //let ``Max for a list of ints can't process a list of strings`` = max ["1"; "2"; "3"] //Thus showing that the current implementation is not generic enough.
+
+//Defining the sum aggregator in terms of folding with an accumulator
+//Now using the the generic fold high order function from the collections module:
+let sum2 inputs = //The structure seems familiar to the imperative version, but the mutation is no longer there
+    Seq.fold //Lint may complain this is the same as Seq.sum, for obvious reasons
+        (fun state input -> state + input)  //Same as declaring the operator directly as a function, like this: (+); That's what the lintter is complaining about.
+        0
+        inputs
+
+//The same tests as before, with the same results
+let ``add 5 and 3 with sum2`` = sum2 [5; 3]
+let ``add 100 and -90 with sum2`` = sum2 [100; -90]
+let ``add -100 and -90 with sum2`` = sum2 [-100; -90]
+let ``add 0 and 0 with sum2`` = sum2 [0; 0]
+//let ``add 1 and null with sum2`` = sum2 [1; null] //Compiler error!
+//let ``add 1 and null take 2 with sum2`` = sum2 [1; int(Nullable<int>(null))] //Compiler error!
+
+//A new version, logging each iteration
+let sum2log inputs = 
+    Seq.fold 
+        (fun state input -> 
+            let newState = state + input
+            printfn "Current state is %02d, input is %02d, and new state value is %02d" state input newState
+            newState)
+        0
+        inputs
+
+//One final test with the log version
+sum2log [1..5]
