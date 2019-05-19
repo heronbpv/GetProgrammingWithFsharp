46033a1d39bdacf66206927d8b8b8c18ddfec09e
heronbpv <heronbpeixoto@gmail.com>
2019-05-19 17:54:03 -0300

    Now you try 22.2.2 resolution.

--- Lesson22/Scratchpad.fsx
+++ Lesson22/Scratchpad.fsx
@@ -14,3 +14,17 @@ let some = calculateAnnualPremiumUsd (Some 250)
 let none = calculateAnnualPremiumUsd None
 let maybe = calculateAnnualPremiumUsd maybeANumber
 //let error = calculateAnnualPremiumUsd aNumber //Passing a non-optional value raises a compiler error.
+
+//@Now you try 22.2.2
+type Customer = { Name:string; Score:int option }
+let customers = [ { Name = "Alice"; Score = None }; { Name = "Bob"; Score = Some 10 } ]
+let calculateAnnualPremiumUsdFromCustomer (customer:Customer) = 
+    match customer.Score with
+    | Some 0 -> 250
+    | Some score when score < 0 -> 400
+    | Some score when score > 0 -> 150
+    | None -> 
+        printfn "No score supplied for '%s'! Using temporary premium." customer.Name
+        300
+
+let results = customers |> List.iter (fun customer -> printfn "Customer: %s; Score: %d" customer.Name (calculateAnnualPremiumUsdFromCustomer customer))
