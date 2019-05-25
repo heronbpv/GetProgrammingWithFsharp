2ed2ff8764fe9567f9d0745f40a16e252476b737
heronbpv <heronbpeixoto@gmail.com>
2019-05-25 08:09:45 -0300

    First foray into making illegal states unrepresentable: adding single case DUs to represent the types.

--- Lesson23/Scratchpad.fsx
+++ Lesson23/Scratchpad.fsx
@@ -1,3 +1,10 @@
+type Address = Address of string
+let myAddress = Address "1 The Street"
+//let isTheSameAddress = myAddress = "1 The Street" //Compiler error -> type mismatch.
+let (Address addressData) = myAddress
+let isTheSameAddress = "1 The Street" = addressData //Correct way of making a comparison, by unwrapping the DU's payload first.
+
+
 type Customer = 
     { CustomerId:string
       Email:string
