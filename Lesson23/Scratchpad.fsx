124d8e96cf20e6d1caff401b7252390ce4835070
heronbpv <heronbpeixoto@gmail.com>
2019-05-25 08:28:43 -0300

    Now you try 23.1.3 resolution. Changed usage of the three separate single case DUs to a three-case one. Made the appropriate changes elsewhere and commented the unused types to generate less confusion.

--- Lesson23/Scratchpad.fsx
+++ Lesson23/Scratchpad.fsx
@@ -1,25 +1,27 @@
-type Address = Address of string
-let myAddress = Address "1 The Street"
-//let isTheSameAddress = myAddress = "1 The Street" //Compiler error -> type mismatch.
-let (Address addressData) = myAddress
-let isTheSameAddress = "1 The Street" = addressData //Correct way of making a comparison, by unwrapping the DU's payload first.
+//type Address = Address of string
+//let myAddress = Address "1 The Street"
+////let isTheSameAddress = myAddress = "1 The Street" //Compiler error -> type mismatch.
+//let (Address addressData) = myAddress
+//let isTheSameAddress = "1 The Street" = addressData //Correct way of making a comparison, by unwrapping the DU's payload first.
 
 //@Now you try 23.1.2
 
 type CustomerId = CustomerId of string
-type Email = Email of string
-type Telephone = Telephone of string
+//type Email = Email of string
+//type Telephone = Telephone of string
+
+//@Now you try 23.1.3
+type ContactDetails =
+    | Address of string
+    | Telephone of string
+    | Email of string
 
 type Customer = 
     { CustomerId:CustomerId
-      Email:Email
-      Telephone:Telephone
-      Address:Address }
+      ContactDetails:ContactDetails }
 
-let createCustomer customerId email telephone address = 
+let createCustomer customerId contactDetails = 
     { CustomerId = customerId
-      Email = email
-      Telephone = telephone
-      Address = address }
+      ContactDetails = contactDetails }
 
-let customer = createCustomer (CustomerId "C-123") (Email "nicki@myemail.com") (Telephone "029-293-23") (Address "1 The Street")
+let customer = createCustomer (CustomerId "C-123") (Email "nicki@myemail.com")
