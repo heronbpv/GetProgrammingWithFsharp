25ad219d758a4d70535838474e76393b79135f3b
heronbpv <heronbpeixoto@gmail.com>
2019-05-26 18:50:38 -0300

    Now you try 26.2.2 resolution. Switched from nuget to paket as the package manager for the project.

--- Lesson26/NuggetFSharp/NuggetFSharp/Scripts/load-references-debug.fsx
+++ Lesson26/NuggetFSharp/NuggetFSharp/Scripts/load-references-debug.fsx
@@ -1,13 +1,14 @@
 // Warning: generated file; your changes could be lost when a new file is generated.
 #I __SOURCE_DIRECTORY__
-#r "../../packages/Humanizer.Core.2.6.2/lib/netstandard1.0/Humanizer.dll"
-#r "../../packages/Newtonsoft.Json.12.0.2/lib/net45/Newtonsoft.Json.dll"
 #r "System.ComponentModel.Composition.dll"
 #r "System.Core.dll"
 #r "System.dll"
 #r "System.IO.Compression.dll"
 #r "System.Net.Http.dll"
 #r "System.Numerics.dll"
-#r "../../packages/System.Runtime.InteropServices.RuntimeInformation.4.3.0/lib/net45/System.Runtime.InteropServices.RuntimeInformation.dll"
 #r "System.Xml.dll"
 #r "System.Xml.Linq.dll"
+#r "../bin/Debug/Humanizer.dll"
+#r "../bin/Debug/Newtonsoft.Json.dll"
+#r "System.Data.dll"
+#r "C:/WINDOWS/Microsoft.Net/assembly/GAC_MSIL/System.Runtime.InteropServices.RuntimeInformation/v4.0_4.0.0.0__b03f5f7f11d50a3a/System.Runtime.InteropServices.RuntimeInformation.dll"
