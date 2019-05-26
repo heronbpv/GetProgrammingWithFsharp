6e476fdc28f18a704bb568c290502e852e91c9a8
heronbpv <heronbpeixoto@gmail.com>
2019-05-26 17:52:06 -0300

    Simplified some directives and added some observations of my own.

--- Lesson26/NuggetFSharp/NuggetFSharp/Script.fsx
+++ Lesson26/NuggetFSharp/NuggetFSharp/Script.fsx
@@ -1,8 +1,9 @@
 ﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
 // for more guidance on F# programming.
-#r @"D:\Programacao\GetProgrammingWithFsharp\Lesson26\NuggetFSharp\packages\Humanizer.Core.2.6.2\lib\netstandard1.0\Humanizer.dll"
-#r @"D:\Programacao\GetProgrammingWithFsharp\Lesson26\NuggetFSharp\packages\Newtonsoft.Json.12.0.2\lib\net45\Newtonsoft.Json.dll"
-#load "Library1.fs"
+#I @"..\packages\" //Using directive #I to add the relative package path to the library include path of fsi, thus simplifying the paths below.
+#r @"Humanizer.Core.2.6.2\lib\netstandard1.0\Humanizer.dll"
+#r @"Newtonsoft.Json.12.0.2\lib\net45\Newtonsoft.Json.dll" //This also makes this script more shareable, since there's no absolute paths here.
+#load "Library1.fs" //Remember that, in order for this directive to compile, you have to import it's dependences in order. Newtonsoft.Json is one such example.
 
 Library1.getPerson()
 
