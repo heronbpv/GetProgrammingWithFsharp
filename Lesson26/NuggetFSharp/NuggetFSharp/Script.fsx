9aec58a70339c03d8c113326aaec85d51b546aee
heronbpv <heronbpeixoto@gmail.com>
2019-05-26 17:33:31 -0300

    Now you try 26.1.2 resolution. Commented some faulty lines on the script.

--- Lesson26/NuggetFSharp/NuggetFSharp/Script.fsx
+++ Lesson26/NuggetFSharp/NuggetFSharp/Script.fsx
@@ -1,8 +1,13 @@
 ﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
 // for more guidance on F# programming.
-
-#load "Library1.fs"
-open NuggetFSharp
+#r @"D:\Programacao\GetProgrammingWithFsharp\Lesson26\NuggetFSharp\packages\Humanizer.Core.2.6.2\lib\netstandard1.0\Humanizer.dll"
+//#load "Library1.fs"
+//open NuggetFSharp
 
 // Define your library scripting code here
-
+//@Now you try 26.1.2
+//Exploring the Humanizer package
+open Humanizer
+let x1 = "ScriptsAreAGreatWayToExplorePackages".Humanize()
+let x2 = "ScriptsAreAGreatWayToExplorePackages
+            ForSureItWouldBeaGoodIdeaAmiRight?".Humanize(LetterCasing.Title)
