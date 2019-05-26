0b3714339219eb9393753400f14e95cdf571c31c
heronbpv <heronbpeixoto@gmail.com>
2019-05-26 18:10:25 -0300

    Now you try 26.1.5 resolution, showcasing the awesomeness of the Generate References for F# Interactive option from the F# Power Tools plugin. very nice, indeed!

---
+++ Lesson26/NuggetFSharp/NuggetFSharp/Scripts/load-references-debug.fsx
@@ -0,0 +1,13 @@
+// Warning: generated file; your changes could be lost when a new file is generated.
+#I __SOURCE_DIRECTORY__
+#r "../../packages/Humanizer.Core.2.6.2/lib/netstandard1.0/Humanizer.dll"
+#r "../../packages/Newtonsoft.Json.12.0.2/lib/net45/Newtonsoft.Json.dll"
+#r "System.ComponentModel.Composition.dll"
+#r "System.Core.dll"
+#r "System.dll"
+#r "System.IO.Compression.dll"
+#r "System.Net.Http.dll"
+#r "System.Numerics.dll"
+#r "../../packages/System.Runtime.InteropServices.RuntimeInformation.4.3.0/lib/net45/System.Runtime.InteropServices.RuntimeInformation.dll"
+#r "System.Xml.dll"
+#r "System.Xml.Linq.dll"
