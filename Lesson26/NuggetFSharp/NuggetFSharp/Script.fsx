abc5042f3fa2f89231a953a64c08062ada8bf21f
heronbpv <heronbpeixoto@gmail.com>
2019-05-26 17:38:09 -0300

    Fixed the error on the #load directive, and added a call to the getPerson function defined in Library1.fs.

--- Lesson26/NuggetFSharp/NuggetFSharp/Script.fsx
+++ Lesson26/NuggetFSharp/NuggetFSharp/Script.fsx
@@ -1,8 +1,10 @@
 ﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 #r @"D:\Programacao\GetProgrammingWithFsharp\Lesson26\NuggetFSharp\packages\Humanizer.Core.2.6.2\lib\netstandard1.0\Humanizer.dll"
-//#load "Library1.fs"
-//open NuggetFSharp
+#r @"D:\Programacao\GetProgrammingWithFsharp\Lesson26\NuggetFSharp\packages\Newtonsoft.Json.12.0.2\lib\net45\Newtonsoft.Json.dll"
+#load "Library1.fs"
+
+Library1.getPerson()
 
 // Define your library scripting code here
 //@Now you try 26.1.2
