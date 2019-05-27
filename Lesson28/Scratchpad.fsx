739ef3d91aceb22d4763656e5f013a23654107ea
heronbpv <heronbpeixoto@gmail.com>
2019-05-26 22:49:14 -0300

    Added a better, more robust domain model, apropriate for f# consumption, then isolated both the old model and the new, respectively into the DTO and Domain modules.

--- Lesson28/Scratchpad.fsx
+++ Lesson28/Scratchpad.fsx
@@ -1,12 +1,33 @@
-open System.Collections.Generic
+///Defines the types to be used at the boundaries of the system.
+module DTO =
+    open System.Collections.Generic
 
-type OrderItemRequest = { ItemId:int; Count:int }
-type OrderRequest = 
-    { OrderId:int
-      CustomerName:string //mandatory
-      Comment:string //optional
-      ///One of (email or telephone), or none
-      EmailUpdates:string
-      TelephneUpdates:string
-      Items:IEnumerable<OrderItemRequest> //mandatory
-    } 
+    type OrderItemRequest = { ItemId:int; Count:int }
+    type OrderRequest = 
+        { OrderId:int
+          CustomerName:string
+          Comment:string
+          ///One of (email or telephone), or none
+          EmailUpdates:string
+          TelephneUpdates:string
+          Items:IEnumerable<OrderItemRequest> }
+
+///The internal domain representation. Inherently more powerful and f# friendly.
+module Domain =
+    type OrderId = OrderId of int
+    type ItemId = ItemId of int
+
+    type OrderItem = { ItemId:ItemId; Count:int }
+    type UpdatePreference = 
+        | EmailUpdates of string
+        | TelephoneUpdates of string
+
+    type Order = 
+        { 
+          OrderId:OrderId
+          CustomerName:string 
+          Comment:string option 
+          ContactPreference:UpdatePreference option
+          Items:OrderItem list
+        } 
+
