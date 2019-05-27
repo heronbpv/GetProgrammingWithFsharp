d286023ce62f2097d5d6654f097626d197794c1a
heronbpv <heronbpeixoto@gmail.com>
2019-05-26 21:03:11 -0300

    Added the Interop solution, with the FSharpCode and CSharpApp projects on it. Made the later reference the former, and built it.

---
+++ Lesson27/Interop/CSharpApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car(4, "Supacar");
+        }
+    }
+}
---
+++ Lesson27/Interop/FSharpCode/Library1.fs
@@ -0,0 +1,10 @@
+﻿namespace Model
+
+///A Standard F# record of a Car.
+type Car =
+    {
+        ///The Number of wheels on the car.
+        Wheels:int
+        ///The brand of the car.
+        Brand:string
+    }
