330af811401609af534679f14ed1358f89a25f88
heronbpv <heronbpeixoto@gmail.com>
2019-05-26 21:31:14 -0300

    Now you try 27.1.3 resolution. Added an tuple to the Car record definition, and a DU to the Model namespace. Then accessed them in the c# code, exposing their properties as local vars, then printing them all to the Console.

--- Lesson27/Interop/CSharpApp/Program.cs
+++ Lesson27/Interop/CSharpApp/Program.cs
@@ -11,7 +11,21 @@ namespace CSharpApp
     {
         static void Main(string[] args)
         {
-            var car = new Car(4, "Supacar");
+            var car = new Car(4, "Supacar", Tuple.Create<double, double>(1.5, 3.5));
+            var wheels = car.Wheels;
+            var brand = car.Brand;
+            var x = car.Dimensions.Item1;
+            var y = car.Dimensions.Item2;
+
+            Console.WriteLine("Car -> Brand {0}; Wheels {1}; Dimension(x, y) ({2}, {3})", brand, wheels, x, y);
+
+            var vehicle = Vehicle.NewMotorbike("Supabike", 12.0);
+            //So, in order to access the properties of a Motorbike type, I have to first explicitly cast to it, since the assignment above leads to a vehicle.
+            var bike = vehicle.IsMotorbike ? vehicle as Vehicle.Motorbike : null; //Yes, this will lead to null eventualy in other circunstances; Never in production!
+            var name = bike.Name;
+            var engineSize = bike.EngineSize;
+
+            Console.WriteLine("Motorbike -> Name {0}; Engine Size {1}", name, engineSize);
         }
     }
 }
--- Lesson27/Interop/FSharpCode/Library1.fs
+++ Lesson27/Interop/FSharpCode/Library1.fs
@@ -1,10 +1,18 @@
 ﻿namespace Model
 
-///A Standard F# record of a Car.
+/// A Standard F# record of a Car.
 type Car =
     {
-        ///The Number of wheels on the car.
+        /// The Number of wheels on the car.
         Wheels:int
-        ///The brand of the car.
+        /// The brand of the car.
         Brand:string
+        /// The x/y of the car in meters
+        Dimensions:float * float
     }
+
+type Vehicle = 
+      /// A car is a type of vehicle.
+    | Motorcar of Car
+      /// A bike is also a type of vehicle
+    | Motorbike of Name:string * EngineSize:float
