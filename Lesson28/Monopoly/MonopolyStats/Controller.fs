8cd121aadf5c745e4941cc9bc0bc0d388014c86c
heronbpv <heronbpeixoto@gmail.com>
2019-05-26 23:43:14 -0300

    Added the monopoly project to the folder.

---
+++ Lesson28/Monopoly/MonopolyStats/Controller.fs
@@ -0,0 +1,145 @@
+﻿namespace Monopoly
+
+open Monopoly.Data
+open System
+
+/// Represents the roll of two dice.
+type DiceData = (int * int)
+
+/// A movement that has occurred for a player.
+type MovementData = 
+    { CurrentPosition : Position
+      DoubleCount : int }
+
+/// A type of movement event.
+type MovementEvent = 
+    /// When a player lands on a position from a throw of the dice.
+    | LandedOn of MovementData * DiceData
+    /// When a player lands on a position as a result of an external event e.g. chance card.
+    | MovedTo of MovementData
+    override this.ToString() =
+        match this with
+        | LandedOn _ -> "Landed on"
+        | MovedTo _ -> "Moved to"
+    member this.MovementData =
+        match this with
+        | LandedOn (movementData, _) -> movementData
+        | MovedTo movementData -> movementData
+
+/// Contains internal Monopoly functionality.
+module Functions =
+    let moveBy dice currentPosition = 
+        let diceValue = fst dice + snd dice
+        let currentIndex = Board |> List.findIndex ((=) currentPosition)
+        let newIndex = currentIndex + diceValue
+        Board.[newIndex % 40]
+    
+    let tryMoveFromDeck deck pickCard position = 
+        match deck |> List.item (pickCard()) with
+        | GoTo destination -> Some destination
+        | Move numberOfSpaces -> Some(position |> moveBy (numberOfSpaces, 0))
+        | Other -> None
+
+    /// Picks a card from the Chance deck.
+    let tryMoveFromChance = tryMoveFromDeck ChanceDeck
+    /// Picks a card from the Community Chest deck.
+    let tryMoveFromCommunityChest = tryMoveFromDeck CommunityChestDeck
+
+    (* These two Active Patterns allow us to reason about the roll of dice a 
+       little more easily so that we can pattern match over some higher
+       abstractions (see below) *)
+    let (|ThreeDoubles|LessThanThreeDoubles|) numberOfDoubles =
+        match numberOfDoubles with
+        | 3 -> ThreeDoubles
+        | _ -> LessThanThreeDoubles
+
+    let (|Double|NotADouble|) dice =
+        match dice with
+        | a,b when a = b -> Double
+        | _ -> NotADouble
+
+    /// Given a Random, returns a function that will generate two dice rolls on each call.
+    let createDiceThrow (random:Random) =
+        let doRoll() = random.Next(1, 7)
+        fun() -> doRoll(), doRoll()
+
+    /// Given a Random, returns a function that will pick a number between 0 and 15
+    let createPickCard (random:Random) = fun () -> random.Next(0, 15)
+
+    /// <summary>
+    /// Plays a single turn, based on the previous movement.
+    /// </summary>
+    /// <param name="doRoll">Performs a roll of a single die.</param>
+    /// <param name="pickCard">Selects a card to pick from 0 to 15.</param>
+    /// <param name="publishMove">Publish a move to some side-effectful listener.</param>
+    /// <param name="moveData">The movement data from the previous turn.</param>
+    /// <returns>Returns a new position and optionally a secondary position (caused by e.g. rolling 3 doubles or chance etc. etc.)</returns>
+    let playTurn rollDice pickCard publishMove moveData =
+        let currentThrow = rollDice()
+        let currentDoubleCount =
+            match (moveData.DoubleCount, currentThrow) with
+            | LessThanThreeDoubles, Double -> moveData.DoubleCount + 1
+            | ThreeDoubles, Double -> 1
+            | LessThanThreeDoubles, NotADouble | ThreeDoubles, NotADouble -> 0
+
+        let generateMovement movingTo throw = 
+            let movementData = { CurrentPosition = movingTo; DoubleCount = currentDoubleCount }
+            let movement =
+                match throw with
+                | Some throw -> LandedOn (movementData, throw)
+                | None -> MovedTo movementData
+            publishMove movement
+            movement
+            
+        match currentDoubleCount with
+        | ThreeDoubles -> generateMovement Jail (Some currentThrow), None
+        | LessThanThreeDoubles -> 
+            let position = moveData.CurrentPosition |> moveBy currentThrow
+            let initialMove = generateMovement position (Some currentThrow)
+            let secondaryMove =
+                match position with
+                | Chance _ -> tryMoveFromChance pickCard position
+                | CommunityChest _ -> tryMoveFromCommunityChest pickCard position
+                | GoToJail -> Some Jail
+                | _ -> None
+
+            match secondaryMove with
+            | Some secondaryMove ->
+                let secondaryMove = generateMovement secondaryMove None
+                initialMove, Some secondaryMove
+            | None -> initialMove, None
+
+    /// <summary>
+    /// Plays a full game.
+    /// </summary>
+    /// <param name="random">Random number generator.</param>
+    /// <param name="turnsToPlay">The number of turns to play for.</param>
+    /// <param name="onMove">Function called whenever a turn is played.</param>
+    /// <param name="seed">Optional random seed.</param>
+    let playGame rollDice pickCard onMove turnsToPlay =
+        /// Plays a single turn.
+        let playTurn = playTurn rollDice pickCard onMove
+
+        let getCurrentPosition (previousTurn:MovementEvent * MovementEvent option) =
+            match previousTurn with
+            | _, Some move | move, None -> move.MovementData
+
+        ((MovedTo { CurrentPosition = Go; DoubleCount = 0 }, None), [ 1 .. turnsToPlay ])
+        ||> List.scan(fun lastTurn _ -> lastTurn |> getCurrentPosition |> playTurn)
+        |> List.collect(fun (mainRoll, optionalSecondaryMove) -> mainRoll :: Option.toList optionalSecondaryMove)
+
+/// Manages a game.
+type Controller() =
+    let onMovedEvent = new Event<MovementEvent>()
+    
+    (* A CLI Event is an event that can be consumed by e.g. C# *)
+    /// Fired whenever a move occurs.
+    [<CLIEvent>]
+    member __.OnMoved = onMovedEvent.Publish
+    
+    /// Plays the game of Monopoly
+    member __.PlayGame turnsToPlay =
+        let random = Random()
+        let rollDice = Functions.createDiceThrow random
+        let pickCard = Functions.createPickCard random
+        Functions.playGame rollDice pickCard onMovedEvent.Trigger turnsToPlay
---
+++ Lesson28/Monopoly/MonopolyStats/Test.fsx
@@ -0,0 +1,33 @@
+﻿(* Use this script to experiment with the Monopoly code *)
+
+#load @"Scripts\load-project-debug.fsx"
+open Monopoly
+
+// Experiment with standalone functions
+Go |> Functions.moveBy (5, 3)
+(Position.Property(Set.Yellow, "Picaddilly")) |> Functions.moveBy (3, 1)
+Functions.playTurn (fun () -> 3, 4) (fun () -> 5) ignore { CurrentPosition = Go; DoubleCount = 0 }
+
+
+
+
+
+// Create a controller to test out the public API
+let controller = Controller()
+
+let printMovementEvent m =
+    match m with
+    | LandedOn (movement, dice) -> sprintf "Rolled a %A and landed on %A (%d doubles)" dice movement.CurrentPosition movement.DoubleCount 
+    | MovedTo movement -> sprintf "Moved to %A (%d doubles)" movement.CurrentPosition movement.DoubleCount
+
+// Optional - subscribe to real-time events. In F# we can use list-like operations (similar to RX) over events
+controller.OnMoved
+|> Event.filter(function MovedTo { CurrentPosition = Jail } -> true | _ -> false)
+|> Event.add(fun _ -> printfn "YOU WENT TO JAIL!")
+
+// Play a number of moves and get the history of the game.
+let history = controller.PlayGame 50
+
+// Print out the history
+history
+|> List.map printMovementEvent
