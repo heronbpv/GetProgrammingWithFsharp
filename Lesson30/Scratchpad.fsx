cc484108e48db768a3a8923cfd8dd5e9d9a4d34a
heronbpv <heronbpeixoto@gmail.com>
2019-06-07 16:04:17 -0300

    Now you try 30.2.2. Loaded the csv file, and send some data to fsi. Now to solve the proposed question.

--- Lesson30/Scratchpad.fsx
+++ Lesson30/Scratchpad.fsx
@@ -1,2 +1,9 @@
 #I @"..\get-programming-fsharp-master\packages"
+//Now you try 30.2.2
 #r @"FSharp.Data\lib\net40\FSharp.Data.dll"
+
+open FSharp.Data
+
+type Football = CsvProvider< @"..\get-programming-fsharp-master\data\FootballResults.csv">
+let data = Football.GetSample().Rows |> Seq.toArray
+data.[0]
