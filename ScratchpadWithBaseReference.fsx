da5a982ae2a408b8edae703cb10ed212a808bf96
heronbpv <heronbpeixoto@gmail.com>
2019-06-07 22:28:04 -0300

    Added the dlls to the base reference list, for ease of use on future scripts.

--- ScratchpadWithBaseReference.fsx
+++ ScratchpadWithBaseReference.fsx
@@ -1,1 +1,5 @@
 #I @"..\get-programming-fsharp-master\packages"
+#r @"FSharp.Data\lib\net40\FSharp.Data.dll"
+#r @"Newtonsoft.Json\lib\net45\Newtonsoft.Json.dll"
+#r @"XPlot.GoogleCharts\lib\net45\XPlot.GoogleCharts.dll"
+#r @"Google.DataTable.Net.Wrapper\lib\Google.DataTable.Net.Wrapper.dll"
