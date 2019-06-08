9611c2a93edbafbc258cb39980696edffd583f20
heronbpv <heronbpeixoto@gmail.com>
2019-06-07 23:32:40 -0300

    Now you try 31.1.2 resolution. Too bad the bbc example in code does not work anymore. They changed how their backend works, so there is no more JSON at the url provided in the example.

--- Lesson31/Scratchpad.fsx
+++ Lesson31/Scratchpad.fsx
@@ -3,3 +3,14 @@
 #r @"Newtonsoft.Json\lib\net45\Newtonsoft.Json.dll"
 #r @"XPlot.GoogleCharts\lib\net45\XPlot.GoogleCharts.dll"
 #r @"Google.DataTable.Net.Wrapper\lib\Google.DataTable.Net.Wrapper.dll"
+open FSharp.Data
+open XPlot.GoogleCharts
+
+//@Now you try 31.1.2
+type Films = HtmlProvider< @"https://en.wikipedia.org/wiki/Robert_De_Niro_filmography">
+let deNiro = Films.GetSample()
+
+deNiro.Tables.Film.Rows
+|> Array.countBy (fun film -> film.Year.ToString())
+|> Chart.SteppedArea
+|> Chart.Show
