1f5f2f6ad33d6f00d95089aaab55ef4f9da35072
heronbpv <heronbpeixoto@gmail.com>
2019-06-08 00:38:49 -0300

    Now you try 31.3.1 resolution. Apparently, Nuget changed the format of its version history table, so the results are different than the ones present in the book (the versions do not contain the name of the package).

--- Lesson31/Scratchpad.fsx
+++ Lesson31/Scratchpad.fsx
@@ -14,3 +14,21 @@ deNiro.Tables.Film.Rows
 |> Array.countBy (fun film -> film.Year.ToString())
 |> Chart.SteppedArea
 |> Chart.Show
+
+//@Now you try 31.3.1
+type Nuget = HtmlProvider< @"..\get-programming-fsharp-master\data\sample-package.html">
+let nuget = Nuget.GetSample()
+//nuget.Tables.``Version History``
+
+let nunit = Nuget.Load @"https://www.nuget.org/packages/nunit"
+//nunit.Tables.``Version History``
+let ef = Nuget.Load @"https://www.nuget.org/packages/EntityFramework"
+let nsjson = Nuget.Load @"https://www.nuget.org/packages/Newtonsoft.Json"
+
+[nunit; ef; nsjson]
+|> Seq.collect (fun package -> package.Tables.``Version History``.Rows)
+|> Seq.sortByDescending (fun versions -> versions.Downloads)
+|> Seq.take 10
+|> Seq.map (fun versions -> versions.Version, versions.Downloads)
+|> Chart.Column
+|> Chart.Show
