80113f20f248a41a70ff8f8422ef75ae76da657a
heronbpv <heronbpeixoto@gmail.com>
2019-06-08 20:48:08 -0300

    Adding just for completeness sake. This lesson is a bummer, the dacpac publishing simply does not work on my machine, at all... :(

---
+++ Lesson32/Scratchpad.fsx
@@ -0,0 +1,21 @@
+(** 
+ * localdb CREATE USER [NT AUTHORITY\NETWORK SERVICE] FOR LOGIN [NT AUTHORITY\NETWORK SERVICE]; 
+ * returns Windows NT user or group 'NT AUTHORITY\NETWORK SERVICE' not found. Check the name again.
+ * Switch the instruction to: 
+CREATE USER [NETWORK SERVICE] 
+            WITHOUT LOGIN 
+            WITH DEFAULT_SCHEMA = dbo 
+GO
+ * Then, change the references to [NT AUTHORITY\NETWORK SERVICE] to [NETWORK SERVICE].
+ * The database is generated empty, though...
+ **)
+
+ #I @"..\get-programming-fsharp-master\packages"
+ #r @"FSharp.Data.SqlClient\lib\net40\FSharp.Data.SqlClient.dll"
+
+ open FSharp.Data
+ let [<Literal>] Conn = "Server=(localdb)\MSSQLLocalDb;Database=AdventureWorksLT;Integrated Security=SSPI"
+ type GetCostumers = SqlCommandProvider<"SELECT * FROM SalesLT.Customer", Conn>
+
+ let customers = GetCostumers.Create(Conn).Execute() |> Seq.toArray
+ let customer = customers.[0]
