a844c2b0dfe669fdcba6df726e3d798b6e55004a
heronbpv <heronbpeixoto@gmail.com>
2019-06-08 23:22:26 -0300

    Now you try 32.2.1, and the workaround was found, hooray! \o/

--- Lesson32/Scratchpad.fsx
+++ Lesson32/Scratchpad.fsx
@@ -12,10 +12,12 @@ GO
 
  #I @"..\get-programming-fsharp-master\packages"
  #r @"FSharp.Data.SqlClient\lib\net40\FSharp.Data.SqlClient.dll"
-
+ //@Now you try 32.2.1
  open FSharp.Data
  let [<Literal>] Conn = "Server=(localdb)\MSSQLLocalDb;Database=AdventureWorksLT;Integrated Security=SSPI"
  type GetCostumers = SqlCommandProvider<"SELECT * FROM SalesLT.Customer", Conn>
 
+ 
  let customers = GetCostumers.Create(Conn).Execute() |> Seq.toArray
  let customer = customers.[0]
+ printfn "%s %s works for %s" customer.FirstName customer.LastName (Option.get customer.CompanyName) //Option.defaultValue seems to not been available... beware exceptions!
