b6ed5aea1c382c25470a614404767d1007fd2b00
heronbpv <heronbpeixoto@gmail.com>
2019-06-09 00:45:30 -0300

    Answers for QC 32.2, plus a demonstration of the SqlDataProvider capabilities. The changes to the base reference script are just the inclusion of the #r directives for the data access type providers used in the lesson.

--- Lesson32/Scratchpad.fsx
+++ Lesson32/Scratchpad.fsx
@@ -3,26 +3,59 @@
  * by following the instructions found here "https://docs.microsoft.com/en-gb/visualstudio/data-tools/install-sql-server-sample-databases?view=vs-2015".
  **)
 
- #I @"..\get-programming-fsharp-master\packages"
- #r @"FSharp.Data.SqlClient\lib\net40\FSharp.Data.SqlClient.dll"
- //@Now you try 32.2.1
- open FSharp.Data
- let [<Literal>] Conn = "Server=(localdb)\MSSQLLocalDb;Database=AdventureWorksLT;Integrated Security=SSPI"
- type GetCostumers = SqlCommandProvider<"SELECT * FROM SalesLT.Customer", Conn>
+#I @"..\get-programming-fsharp-master\packages"
+#r @"FSharp.Data.SqlClient\lib\net40\FSharp.Data.SqlClient.dll"
+//Using the SqlClient
+//@Now you try 32.2.1
+open FSharp.Data
+let [<Literal>] Conn = "Server=(localdb)\MSSQLLocalDb;Database=AdventureWorksLT;Integrated Security=SSPI"
+type GetCostumers = SqlCommandProvider<"SELECT * FROM SalesLT.Customer", Conn>
 
  
- let customers = GetCostumers.Create(Conn).Execute() |> Seq.toArray
- let customer = customers.[0]
- printfn "%s %s works for %s" customer.FirstName customer.LastName (Option.get customer.CompanyName) //Option.defaultValue seems to not been available... beware exceptions!
+let customers = GetCostumers.Create(Conn).Execute() |> Seq.toArray
+let customer = customers.[0]
+printfn "%s %s works for %s" customer.FirstName customer.LastName (Option.get customer.CompanyName) //Option.defaultValue seems to not been available... beware exceptions!
 
- //@Now you try 32.2.2
- type AdventureWorks = SqlProgrammabilityProvider<Conn>
- let ProductCategory = new AdventureWorks.SalesLT.Tables.ProductCategory()
- ProductCategory.AddRow("Mittens", Some 3)
- ProductCategory.AddRow("Long Shorts", Some 3)
- ProductCategory.AddRow("Wooly Hats", Some 4)
- ProductCategory.Update()
+//@Now you try 32.2.2
+type AdventureWorks = SqlProgrammabilityProvider<Conn>
+let ProductCategory = new AdventureWorks.SalesLT.Tables.ProductCategory()
+ProductCategory.AddRow("Mittens", Some 3)
+ProductCategory.AddRow("Long Shorts", Some 3)
+ProductCategory.AddRow("Wooly Hats", Some 4)
+ProductCategory.Update()
 
- type Categories = SqlEnumProvider<"SELECT Name, ProductCategoryId FROM SalesLT.ProductCategory", Conn>
- let woolyHats = Categories.``Wooly Hats``
- printfn "Wooly Hats has ID %d" woolyHats
+type Categories = SqlEnumProvider<"SELECT Name, ProductCategoryId FROM SalesLT.ProductCategory", Conn>
+let woolyHats = Categories.``Wooly Hats``
+printfn "Wooly Hats has ID %d" woolyHats
+
+
+//Using the SqlProvider
+#r @"SQLProvider\lib\FSharp.Data.SqlProvider.dll"
+open FSharp.Data.Sql
+type AdventureWorks2 = SqlDataProvider<ConnectionString = Conn, UseOptionTypes = true>
+let context = AdventureWorks2.GetDataContext()
+
+let customers2 =
+    query {
+        for customer in context.SalesLt.Customer do
+        take 10
+    }
+    |> Seq.toArray
+
+let customer2 = customers2.[0]
+
+printfn "%s %s works for %s" customer2.FirstName customer2.LastName (Option.get customer2.CompanyName) //Same results as before
+
+let result =
+    query {
+        for customer in context.SalesLt.Customer do
+        where (customer.CompanyName = Some "Sharp Bikes")
+        select (customer.FirstName, customer.LastName)
+        distinct
+    }
+    |> Seq.toArray //This is necessary to convert from the IQueryable type that's the result of the query expression
+
+let category = context.SalesLt.ProductCategory.Create()
+category.ParentProductCategoryId <- Some 3
+category.Name <- "Scarf"
+context.SubmitUpdates()
--- ScratchpadWithBaseReference.fsx
+++ ScratchpadWithBaseReference.fsx
@@ -3,3 +3,5 @@
 #r @"Newtonsoft.Json\lib\net45\Newtonsoft.Json.dll"
 #r @"XPlot.GoogleCharts\lib\net45\XPlot.GoogleCharts.dll"
 #r @"Google.DataTable.Net.Wrapper\lib\Google.DataTable.Net.Wrapper.dll"
+#r @"FSharp.Data.SqlClient\lib\net40\FSharp.Data.SqlClient.dll"
+#r @"SQLProvider\lib\FSharp.Data.SqlProvider.dll"
