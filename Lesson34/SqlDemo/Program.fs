3c9e1fc60d82764ee50c5256a6dae905a04a1ac3
heronbpv <heronbpeixoto@gmail.com>
2019-06-17 14:25:17 -0300

    Now you try 34.1.1. Modified the type provider configuration in Program.fs to look up to the db name in the App.config file.

--- Lesson34/SqlDemo/Program.fs
+++ Lesson34/SqlDemo/Program.fs
@@ -1,11 +1,11 @@
 ﻿open FSharp.Data
 
-let [<Literal>] Conn = "Server=(localdb)\MSSQLLocalDb;Database=AdventureWorksLT;Integrated Security=SSPI"
-type GetCustomers = SqlCommandProvider<"SELECT TOP 50 * FROM SalesLT.Customer", Conn>
+//let [<Literal>] Conn = "Server=(localdb)\MSSQLLocalDb;Database=AdventureWorksLT;Integrated Security=SSPI"
+type GetCustomers = SqlCommandProvider<"SELECT TOP 50 * FROM SalesLT.Customer", "Name=AdventureWorks">
 
 [<EntryPoint>]
 let main _ = 
-    let customers = GetCustomers.Create(Conn)
+    let customers = GetCustomers.Create()
     
     customers.Execute()
     |> Seq.iter (fun c -> printfn "%A: %s %s" c.CompanyName c.FirstName c.LastName)
