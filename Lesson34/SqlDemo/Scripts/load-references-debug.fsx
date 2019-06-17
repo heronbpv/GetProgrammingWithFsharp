2c4fbbb62d0e022584522a439a1ede89c661a928
heronbpv <heronbpeixoto@gmail.com>
2019-06-17 14:01:29 -0300

    Added the referenced sample project, updated the reference for FSharp.Data.SqlClient, then built it.

---
+++ Lesson34/SqlDemo/Scripts/load-references-debug.fsx
@@ -0,0 +1,9 @@
+// Warning: generated file; your changes could be lost when a new file is generated.
+#I __SOURCE_DIRECTORY__
+#r "System.Configuration.dll"
+#r "System.Core.dll"
+#r "System.Data.dll"
+#r "System.dll"
+#r "System.Numerics.dll"
+#r "C:/Program Files (x86)/Microsoft SQL Server/130/SDK/Assemblies/Microsoft.SqlServer.TransactSql.ScriptDom.dll"
+#r "../../../get-programming-fsharp-master/packages/FSharp.Data.SqlClient/lib/net40/FSharp.Data.SqlClient.dll"
