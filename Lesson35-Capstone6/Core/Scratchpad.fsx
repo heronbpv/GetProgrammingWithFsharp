187ea7a2733e5057adb58d70125fd7c3781adba3
heronbpv <heronbpeixoto@gmail.com>
2019-06-17 16:40:43 -0300

    Now you try 35.3.2. Implemented the function getAccountAndTransactions, then did some tests. Also, increased the timeout on the connection string to 6000 instead of 60, to avoid have to build everytime to get rid of the red squigles from connection error.

--- Lesson35-Capstone6/Core/Scratchpad.fsx
+++ Lesson35-Capstone6/Core/Scratchpad.fsx
@@ -24,3 +24,34 @@ let accountId = GetAccountId.Create(Conn).Execute("isaac")
 let transactions = FindTransactions.Create(Conn).Execute(accountId.Value) |> Seq.toArray
 let transactionsByOwner = FindTransactionsByOwner.Create(Conn).Execute("isaac") |> Seq.toArray
 let operations = DbOperations.Items
+
+let getAccountAndTransactions (owner:string) : (Guid * Transaction seq) option =
+    let transactionsByOwner = FindTransactionsByOwner.Create(Conn).Execute(owner) |> Seq.toList
+    match transactionsByOwner with
+    | [] -> 
+        None
+    | [ transaction ] when (transaction.Amount, transaction.OperationId, transaction.Timestamp) = (None, None, None) ->
+        Some (transaction.AccountId, seq [])
+    | x :: xs ->
+        let transactions = 
+            let getOperation operationId = 
+                match operationId with
+                | DbOperations.Withdraw -> Withdraw
+                | DbOperations.Deposit -> Deposit
+                | x -> failwith (sprintf "Invalid operation Id '%i' from database." x)
+            seq {
+                yield { Timestamp = defaultArg x.Timestamp DateTime.MinValue
+                        //The book mentions hardcoding the operation as deposit, instead of converting like here, so assume the default to be 2 here.
+                        Operation = getOperation (defaultArg x.OperationId 2) 
+                        Amount = defaultArg x.Amount 0M }
+                for transaction in xs do
+                    yield { Timestamp = defaultArg transaction.Timestamp DateTime.MinValue
+                            Operation = getOperation (defaultArg transaction.OperationId 2)
+                            Amount = defaultArg transaction.Amount 0M }
+            }
+        Some (x.AccountId, transactions)
+
+//Testing the function
+getAccountAndTransactions "isaac"
+getAccountAndTransactions "heron"
+getAccountAndTransactions null //No exception thrown at runtime. Nice!
--- Lesson35-Capstone6/Core/SqlRepository.fs
+++ Lesson35-Capstone6/Core/SqlRepository.fs
@@ -15,7 +15,30 @@ module private DB =
 
 
 let getAccountAndTransactions (owner:string) : (Guid * Transaction seq) option =
-    None
+    let transactionsByOwner = FindTransactionsByOwner.Create(Conn).Execute(owner) |> Seq.toList
+    match transactionsByOwner with
+    | [] -> 
+        None
+    | [ transaction ] when (transaction.Amount, transaction.OperationId, transaction.Timestamp) = (None, None, None) ->
+        Some (transaction.AccountId, seq [])
+    | x :: xs ->
+        let transactions = 
+            let getOperation operationId = 
+                match operationId with
+                | DbOperations.Withdraw -> Withdraw
+                | DbOperations.Deposit -> Deposit
+                | x -> failwith (sprintf "Invalid operation Id '%i' from database." x)
+            seq {
+                yield { Timestamp = defaultArg x.Timestamp DateTime.MinValue
+                        //The book mentions hardcoding the operation as deposit, instead of converting like here, so assume the default to be 2 here.
+                        Operation = getOperation (defaultArg x.OperationId 2) 
+                        Amount = defaultArg x.Amount 0M }
+                for transaction in xs do
+                    yield { Timestamp = defaultArg transaction.Timestamp DateTime.MinValue
+                            Operation = getOperation (defaultArg transaction.OperationId 2)
+                            Amount = defaultArg transaction.Amount 0M }
+            }
+        Some (x.AccountId, transactions)
 
 let writeTransaction (accountId:Guid) (owner:string) (transaction:Transaction) =
     ()
