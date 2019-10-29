ed77ec3942ce2ce5211ef3f46bd3e5e447cbc6b5
heronbpv <heronbpeixoto@gmail.com>
2019-10-29 17:54:42 -0300

    Now you try 36.54 resolution. Ended up having to configure the ServicePointManager in order for the WebClient class to access an http page. The joys of modern web programming...

--- Lesson36/Scratchpad.fsx
+++ Lesson36/Scratchpad.fsx
@@ -42,3 +42,35 @@ let printHelloWorldAsync =
         let! text = getTextAsync
         return printfn "%s WORLD" text
     }
+printHelloWorldAsync |> Async.Start
+
+let random = System.Random()
+let pickANumberAsync = async { return random.Next 10 }
+let createFiftyNumbers = 
+    let workflows = [ for i in 1 .. 10 -> pickANumberAsync ]
+    async {
+        let! numbers = workflows |> Async.Parallel
+        return printfn "Total is %d" (numbers |> Array.sum)
+    }
+createFiftyNumbers |> Async.Start
+
+//@Now you try 36.4
+open System.Net
+///Downloads a page, then returns it's size.
+let downloadData url = 
+    //See https://social.msdn.microsoft.com/Forums/pt-BR/ad198bca-a5b5-4f62-aab9-e3fa0e75f7ee/webclient-erroquota-solicitao-foi-anulada-no-foi-possvel-criar-um-canal-seguro-para?forum=vsvbasicpt
+    //Seems to be some sort of configuration that must be done to avoid errors accessing http pages (a.k.a. pages without a certificate).
+    ServicePointManager.SecurityProtocol <- SecurityProtocolType.Tls12 ||| SecurityProtocolType.Ssl3
+    async {
+        let uri = new System.Uri(url)
+        use client = new WebClient()
+        let! page = client.AsyncDownloadData uri
+        return page.Length
+    }
+
+//This pipeline will download the pages in parallel, then sum up their sizes, in a manner akin to a fork/join strategy.
+[|"http://www.fsharp.org"; "http://microsoft.com"; "http://fsharpforfunandprofit.com"|]
+|> Array.map downloadData
+|> Async.Parallel //Fork part of the pipeline
+|> Async.RunSynchronously
+|> Array.sum //Join part of the pipeline
