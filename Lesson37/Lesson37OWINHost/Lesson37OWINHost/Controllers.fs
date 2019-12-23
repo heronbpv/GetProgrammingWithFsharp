4bcdaf4a6fbcf78ff2bd3545e5c8239d7b16f1b6
heronbpv <heronbpeixoto@gmail.com>
2019-12-23 18:13:39 -0300

    Added the necessary changes prior to the now you try 37.2.1.

--- Lesson37/Lesson37OWINHost/Lesson37OWINHost/Controllers.fs
+++ Lesson37/Lesson37OWINHost/Lesson37OWINHost/Controllers.fs
@@ -1,17 +1,32 @@
 ﻿namespace Controllers
 
+open System.Net
+open System.Net.Http
 open System.Web.Http
 
 //Creating a type from which to expose data
 type Animal = { Name:string; Species:string }
+
+module AnimalsRepository =
+    let all =
+        [
+            { Name = "Fido"; Species = "Dog" }
+            { Name = "Felix"; Species = "Cat" }
+        ]
+    let getAll() = all
+    let getAnimal name = all |> List.tryFind (fun r -> r.Name = name)
+
+[<AutoOpen>]
+module Helpers =
+    let asResponse (request:HttpRequestMessage) result =
+        match result with
+        | Some result -> request.CreateResponse(HttpStatusCode.OK, result)
+        | None -> request.CreateResponse(HttpStatusCode.NotFound)
 
 [<RoutePrefix("api")>] //Setting the Web API route prefix
 type AnimalsController() = 
     inherit ApiController()
 
     [<Route("animals")>] //Web API route
-    member __.Get() = //Defining the GET verb request handler for the api/animals route
-        [
-            { Name = "Fido"; Species = "Dog" }
-            { Name = "Felix"; Species = "Cat" }
-        ]
+    member this.Get(name) = //Defining the GET verb request handler for the api/animals route
+        AnimalsRepository.getAnimal name |> (asResponse this.Request)
